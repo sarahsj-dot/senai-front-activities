069b39a01e841d200a1c9261958e4fce01fa8703
sarah <sarah.sdejesusp@gmail.com>
2026-03-09 21:32:55 -0300

    feat: aula 01 - Introdução ao JavaScript

---
+++ atv-one/app.js
@@ -0,0 +1,6 @@
+        const nome = "Sarah";
+        const idade = 19;
+        const soma = 7 + 3;
+
+console.log("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.");
+console.log("7 + 3 = " + soma);

1fa1dc79533b72fe474262ee2be91ca4d8f8823e
sarah <sarah.sdejesusp@gmail.com>
2026-03-09 21:56:38 -0300

    feat: aula 03 - Variáveis e Operadores

---
+++ atv-three/app.js
@@ -0,0 +1,17 @@
+const nome  = "Sarah";
+const idade = 19;
+const hobby = "Ler";
+
+const n1 = 1;
+const n2 = 9;
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos");
+
+const soma = n1 + n2;
+const subtracao = n2 - n1;
+const multiplicacao = n1 * n2;
+const divisao = n2 / n1;
+
+console.log("A soma de " + n1 + " e " + n2 + " é: " + soma);
+console.log("A subtração de " + n2 + " e " + n1 + " é: " + subtracao);
+console.log("A multiplicação de " + n1 + " e " + n2 + " é: " + multiplicacao);
+console.log("A divisão de " + n2 + " por " + n1 + " é: " + divisao);    
